ed727ae7f79fff27a88f1c56450b83ae75837b1b
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2026-07-02 16:35:10 +0530

    maxnum

--- array/MaxNumArray.js
+++ array/MaxNumArray.js
@@ -1,12 +1,18 @@
-function isMax(arr) {
-  let max = arr[0];
+// function isMax(arr) {
+//   let max = arr[0];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
 
-console.log(isMax([23, 65, 87, 53, 80, 42]));
+let arr = [1, 2, 3, 4];
+let sum = arr.reduce((acc, curr) => {
+  return acc + curr;
+}, 0);
+console.log(sum);
+
+// console.log(isMax([23, 65, 87, 53, 80, 42]));
