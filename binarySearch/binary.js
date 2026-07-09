f57cbcfcd3e4fd9b2344f8d5011ed7fb793df6c5
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2026-07-09 18:29:59 +0530

    add binary search implementation with detailed logging

---
+++ binarySearch/binary.js
@@ -0,0 +1,18 @@
+let arr = [1, 2, 3, 4, 5, 6, 7];
+if (index === -1) console.log("Not Found");
+else console.log("Target Found at index:", index);
+
+let index = binarySearch(arr, 0, arr.length - 1, 5);
+
+function binarySearch(arr, first, last, target) {
+  while (first <= last) {
+    let mid = Math.floor((first + last) / 2);
+    if (arr[mid] == target) return mid;
+    else if (arr[mid] > target) {
+      last = mid - 1;
+    } else {
+      first = mid + 1;
+    }
+  }
+  return -1;
+}
---
+++ binarySearch/binarysearch.js
@@ -0,0 +1,60 @@
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log("Array:", arr);
+
+let index = binarySearch(arr, 0, arr.length - 1, 5);
+
+console.log("Returned Index:", index);
+
+if (index === -1)
+    console.log("Not Found");
+else
+    console.log("Target Found at index:", index);
+
+function binarySearch(arr, first, last, target) {
+
+    console.log("First =", first);
+    console.log("Last =", last);
+    console.log("Target =", target);
+
+    while (first <= last) {
+
+        console.log("\n---------------------------");
+        console.log("Current first =", first);
+        console.log("Current last =", last);
+
+        let mid = Math.floor((first + last) / 2);
+
+        console.log("Mid =", mid);
+        console.log("Middle Value =", arr[mid]);
+
+        if (arr[mid] == target) {
+            console.log("✅ Target Found!");
+            return mid;
+        }
+
+        else if (arr[mid] > target) {
+
+            console.log(arr[mid], ">", target);
+            console.log("Move Left");
+
+            last = mid - 1;
+
+            console.log("New last =", last);
+        }
+
+        else {
+
+            console.log(arr[mid], "<", target);
+            console.log("Move Right");
+
+            first = mid + 1;
+
+            console.log("New first =", first);
+        }
+    }
+
+    console.log("Target Not Found");
+
+    return -1;
+}
