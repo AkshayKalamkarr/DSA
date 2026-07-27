abdcdda4a95da903ecadc5eda9e4b1943ec9c5d3
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2026-07-27 18:25:19 +0530

    fix: reorder index check and add console log for mid calculation

--- binarySearch/binary.js
+++ binarySearch/binary.js
@@ -1,12 +1,14 @@
 let arr = [1, 2, 3, 4, 5, 6, 7];
-if (index === -1) console.log("Not Found");
-else console.log("Target Found at index:", index);
 
 let index = binarySearch(arr, 0, arr.length - 1, 5);
+
+if (index == -1) console.log("Not Found");
+else console.log("Target Found at index:", index);
 
 function binarySearch(arr, first, last, target) {
   if (first <= last) {
     let mid = Math.floor((first + last) / 2);
+    console.log(mid)
     if (arr[mid] == target) {
       return mid;
     } else if (arr[mid] > target) {
